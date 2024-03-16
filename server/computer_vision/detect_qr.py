de57e8fcf7b016c1e7727382ee4ce92e30884781
aswarbs <amberswarbrick1@gmail.com>
2024-03-16 19:14:54 +0000

    add triangulation test

--- server/computer_vision/detect_qr.py
+++ server/computer_vision/detect_qr.py
@@ -3,8 +3,11 @@ import cv2
 import numpy as np
 import sympy as sym
 from pyzbar import pyzbar
+import math
 
 class DetectQR:
+
+
    
 
     def __init__(self, localisation):
@@ -14,6 +17,89 @@ class DetectQR:
         self.offset = self.localisation.border_width
 
 
+
+        self.orientation = math.pi/2
+
+        self.arena_width = 250
+        self.arena_height = 250
+        self.qr_width = 30
+
+        self.position = (int(self.arena_width / 2),int(self.arena_height / 2))
+
+        self.lower_bound_x = 0
+        self.lower_bound_y = 0
+        self.upper_bound_x = self.arena_width
+        self.upper_bound_y = self.arena_height
+
+        self.arena_offset = 0 # arena is shifted 25 to the left
+
+        self.top_left_boundary = (self.arena_offset,self.arena_height + self.arena_offset)
+        self.top_right_boundary = (self.arena_width + self.arena_offset, self.arena_height + self.arena_offset)
+        self.bottom_left_boundary = (self.arena_offset,self.arena_offset)
+        self.bottom_right_boundary = (self.arena_width + self.arena_offset,self.arena_offset)
+
+        self.top_left_facing_south_position = (15, 0)
+        self.top_left_facing_east_position = (0, 15)
+
+        self.top_right_facing_south_position = (235, 0)
+        self.top_right_facing_west_position = (250, 15)
+
+        self.bottom_left_facing_north_position = (15, 250)
+        self.bottom_left_facing_east_position = (0, 15)
+
+        self.bottom_right_facing_north_position = (235, 250)
+        self.bottom_right_facing_west_position = (250, 235)
+
+        self.time_difference = 0
+        self.velocity = 0
+        self.angular_velocity = 0
+        self.robot_size = 9
+        self.border_width = 10
+
+        self.frame_rate = 0.1
+
+        self.labels_to_coordinates = {
+            "top_left_facing_south": self.top_left_facing_south_position,
+            "top_left_facing_east": self.top_left_facing_east_position,
+            "top_right_facing_south": self.top_right_facing_south_position,
+            "top_right_facing_west": self.top_right_facing_west_position,
+            "bottom_left_facing_north": self.bottom_left_facing_north_position,
+            "bottom_left_facing_east":  self.bottom_left_facing_east_position,
+            "bottom_right_facing_north": self.bottom_right_facing_north_position,
+            "bottom_right_facing_west": self.bottom_right_facing_west_position
+        }
+
+
+
+
+
+    def triangulate_position(self, p1, p2, d1, d2):
+        x,y = sym.symbols('x,y')
+        eq1 = sym.Eq((x - p1[0])**2 + (y - p1[1])**2, d1**2)
+        eq2 = sym.Eq((x - p2[0])**2 + (y - p2[1])**2, d2**2)
+        results = sym.solve([eq1,eq2],(x,y))
+
+        valid_results = []
+
+        for x,y in results:
+
+            if (not x.is_real) or (not y.is_real):
+                return
+            
+            if x < self.lower_bound_x or x > self.upper_bound_x:
+                continue
+            if y < self.lower_bound_y or y > self.upper_bound_y:
+                continue
+            valid_results.append((int(x), int(y)))
+
+        if(len(valid_results) == 0):
+            return
+
+        assert(len(valid_results) == 1)
+
+        result = valid_results[0]
+        return result
+    
 
     def calculate_width(self, points):
 
@@ -27,6 +113,23 @@ class DetectQR:
 
 
 
+    def find_position(self, labels_to_distances):
+
+        points = []
+        distances = []
+
+        for key, value in labels_to_distances.items():
+            points.append(self.labels_to_coordinates[key])
+            distances.append(value)
+
+        triangulation = self.triangulate_position(points[0], points[1], distances[0], distances[1])
+
+        if(triangulation is None):
+            return 
+        
+        position = (self.border_width + triangulation[0],self.border_width + (self.arena_height - triangulation[1]))
+
+        return position
              
 
         
@@ -70,7 +173,14 @@ class DetectQR:
             labels_to_distances[label] = approx_distance
 
             
+
+    
+
+            
         if(len(labels_to_distances)) == 2:
-            self.localisation.find_position(labels_to_distances)
+            #self.localisation.find_position(labels_to_distances)
+            position = self.find_position(labels_to_distances)
+            if position is not None:
+                cv2.putText(frame, f"position: {position[0]} {position[1]}", (10,10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 1, cv2.LINE_AA)
 
         return labels_to_distances
